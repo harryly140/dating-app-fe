279e7bf622a9753fdb52aff76d1c3284505e5f48
Harry <harry.ly@ibm.com>
2023-02-15 11:14:43 -0600

    Added styles to the member card

--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 import { Member } from 'src/app/_models/member';
 
 @Component({
   selector: 'app-member-card',
   templateUrl: './member-card.component.html',
   styleUrls: ['./member-card.component.css']
+  // encapsulation: ViewEncapsulation.None this makes it global or not 
 })
 export class MemberCardComponent implements OnInit{
   @Input() member: Member | undefined;
