b01fdb5d23e3e14f21196551a16345539ff3a094
Harry <harry.ly@ibm.com>
2023-02-27 12:49:47 -0600

    Using Route Resolvers

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -13,8 +13,8 @@ import { MessageService } from 'src/app/_services/message.service';
   styleUrls: ['./member-detail.component.css']
 })
 export class MemberDetailComponent implements OnInit{
-  @ViewChild('memberTabs') memberTabs?: TabsetComponent;
-  member: Member | undefined;
+  @ViewChild('memberTabs', {static: true}) memberTabs?: TabsetComponent; // Static makes it so we don't have to wait for view to load
+  member: Member = {} as Member;
   galleryOptions: NgxGalleryOptions[] = [];
   galleryImages: NgxGalleryImage[] = [];
   activeTab?: TabDirective;
@@ -23,7 +23,10 @@ export class MemberDetailComponent implements OnInit{
   constructor(private memberService: MembersService, private route: ActivatedRoute, private messageService: MessageService) { }
 
   ngOnInit(): void {
-    this.loadMember();
+    this.route.data.subscribe({
+      next: data => this.member = data['member']
+    })
+
     this.route.queryParams.subscribe({
       next: params => {
         [params['tab'] && this.selectTab(params['tab'])]
@@ -40,6 +43,7 @@ export class MemberDetailComponent implements OnInit{
         preview: false
       }
     ];
+    this.galleryImages = this.getImages();
   }
 
   getImages() {
@@ -55,16 +59,16 @@ export class MemberDetailComponent implements OnInit{
     return imageUrls;
   }
 
-  loadMember() {
-    const username = this.route.snapshot.paramMap.get('username');
-    if(!username) return;
-    this.memberService.getMember(username).subscribe({
-      next: member => {
-        this.member = member;
-        this.galleryImages = this.getImages();
-      }
-    })
-  }
+  // loadMember() {
+  //   const username = this.route.snapshot.paramMap.get('username');
+  //   if(!username) return;
+  //   this.memberService.getMember(username).subscribe({
+  //     next: member => {
+  //       this.member = member;
+  //       this.galleryImages = this.getImages();
+  //     }
+  //   })
+  // }
 
   selectTab(heading: string) {
     if(this.memberTabs) {
