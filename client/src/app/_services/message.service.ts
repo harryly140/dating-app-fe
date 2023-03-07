9323e8c822686904912dc11d0170ab2f6f7a096c
Harry <harry.ly@ibm.com>
2023-03-07 14:02:35 -0600

    Sending the messages via the hub

--- client/src/app/_services/message.service.ts
+++ client/src/app/_services/message.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, take } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Message } from '../_models/message';
 import { User } from '../_models/user';
@@ -31,6 +31,14 @@ export class MessageService {
     this.hubConnection.on('ReceiveMessageThread', messages => {
       this.messageThreadSource.next(messages);
     })
+
+    this.hubConnection.on('NewMessage', message => {
+      this.messageThread$.pipe(take(1)).subscribe({
+        next: messages => {
+          this.messageThreadSource.next([...messages, message])
+        }
+      })
+    })
   }
 
   stopHubConnection() {
@@ -50,8 +58,10 @@ export class MessageService {
     return this.http.get<Message[]>(this.baseUrl + 'messages/thread/' + username);
   }
 
-  sendMessage(username: string, content: string) {
-    return this.http.post<Message>(this.baseUrl + 'messages', {recipientUsername: username, content}) // Has to match CreateMessageDto in BE
+  async sendMessage(username: string, content: string) {
+    // return this.http.post<Message>(this.baseUrl + 'messages', {recipientUsername: username, content}) // Has to match CreateMessageDto in BE
+    return this.hubConnection?.invoke('SendMessage', {recipientUsername: username, content})
+      .catch(error => console.log(error));
   }
 
   deleteMessage(id: number) {
